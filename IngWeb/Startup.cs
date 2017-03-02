b6aa5b6fac26e246dfea8612473d335feef2e93d
cmondino <cmondino@EOS-CMONDINO>
2017-03-02 23:12:16 +0100

    Aggiungere i file di progetto.

---
+++ IngWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IngWeb.Startup))]
+namespace IngWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
